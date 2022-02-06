02ff3105be251a99b7e6a1a3846901cee52e3f02
vivit <kenji@vivitjp.com>
2022-02-06 09:22:46 +0900

    First Apollo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+          }`
+      }
+    });
+  }
+});
+
+const query = gql`
+  {
+    organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          id
+          name
+          url
+          viewerHasStarred
+          stargazers {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+`
+
+client
+  .query({
+    query
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
